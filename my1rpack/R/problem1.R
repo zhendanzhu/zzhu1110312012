c4eccb096ae30a427a7afa6a02c5a74124246a45
zhendanzhu <zhendanzhu@hotmail.com>
2012-12-07 15:26:37 -0500

    redo HW2

--- my1rpack/R/problem1.R
+++ my1rpack/R/problem1.R
@@ -1,14 +1,16 @@
 problem1 <- function(filename=NULL, fcnname=NULL, nrange=NULL, nsample=NULL, compile=NULL)
 {
+  require("tikzDevice")
   outfilename = paste(filename, '.tex', sep = "")
-  tikz(outfilename, standAlone = TRUE)
-  
+ 
   xbeg = nrange[1]
   xend = nrange[2]
   
   xval = seq(xbeg, xend, length = nsample)
   
   yval = fcnname(xval)
+  
+  tikz(outfilename, standAlone = TRUE)
   
   ylabel = paste("$", deparse(substitute(fcnname)), "(x)$", sep = "")
   plot(xval, yval, type = 'l', ylab = ylabel, xlab = '$x$')
